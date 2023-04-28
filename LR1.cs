c5cf0d7c08abd96785de704071c06fe3ffe6e817
prostodem0n <71063649+gogolp@users.noreply.github.com>
2023-04-28 18:06:33 +0300

    Add files via upload
    Add file

---
+++ LR1.cs
@@ -0,0 +1,26 @@
+using System;
+
+
+namespace example1
+
+{
+    
+	class example1
+        
+	{
+            
+		static void Main()
+		{
+	Console.WriteLine("Прізвище та ініціали: Дем'янчук В.В.");
+                		Console.WriteLine("факультет: ФІТ");
+                			Console.WriteLine("Спеціальність: 121(Інженерія програмного забезпечення)");
+
+			Console.WriteLine("Форма навчання: Денна");
+                			Console.WriteLine("Група: 1-2");
+
+			Console.ReadKey();
+		}
+
+	}
+
+}
